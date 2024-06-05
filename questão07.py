59e36838fff5d7b45a4746b25a5f75e5b16679b3
AIsaac10 <168681995+AIsaac10@users.noreply.github.com>
2024-06-05 10:14:11 -0300

    Update questão07.py

--- questão07.py
+++ questão07.py
@@ -4,6 +4,7 @@ nota1 = float(input("digite a nota parcial: "))
 nota2 = float(input("digite a nota bimestral: "))
 media = ((nota1 + nota2) / 2)
 if media < 6:
-    print(f'O aluno {aluno} está "reprovado" na matéria {dicip} com média {media}')
+   status = (f'reprovado')
 else:
-    print(f'O aluno {aluno} está "aprovado" na matéria {dicip} com média {media}')
+    status = (f'aprovado')
+print(f'{aluno} está {status} na disciplina {dicip}')
