fd065cd6a0a5da996b51bca0186f5403c90bd485
AIsaac10 <168681995+AIsaac10@users.noreply.github.com>
2024-06-05 10:04:12 -0300

    Update questão05.py

--- questão05.py
+++ questão05.py
@@ -2,9 +2,9 @@ hora = int(input("digite um horário: "))
 if hora < 0 or hora > 23:
     print("digite um número entre 0 e 23")
 else:
-    if  4 < hora < 12:
+    if  0 <= hora <= 11:
         print("está de manhã")
-    elif 11 < hora < 18:
+    elif 12 <= hora <= 17:
         print("está de tarde")
     else:
         print("está de noite")
